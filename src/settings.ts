4f71da7a325f8f989986f6f081cae6df39c40177
Andrii Rudenko <kolobock@gmail.com>
2021-03-27 21:54:49 +0200

    plugin name

--- src/settings.ts
+++ src/settings.ts
@@ -6,4 +6,4 @@ export const PLATFORM_NAME = 'OverdaUranus';
 /**
  * This must match the name of your plugin as defined the package.json
  */
-export const PLUGIN_NAME = 'homebridge-overda-uranuss';
+export const PLUGIN_NAME = 'homebridge-overda-uranus';
